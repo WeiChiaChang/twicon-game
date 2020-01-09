34b6d354f603e5615b3b1ecbb59ccfff795369c8
WeiChiaChang <ioh04019@gmail.com>
2020-01-09 15:38:38 +0800

    Initial dist subtree commit

---
+++ precache-manifest.969573fe0747233e72d0ef7d4c491bb4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2530efdd05c43cdf958",
+    "url": "css/app.a83c05ee.css"
+  },
+  {
+    "revision": "f2530efdd05c43cdf958",
+    "url": "js/app.9ab38fe4.js"
+  },
+  {
+    "revision": "99af388e1f171f34fb18",
+    "url": "css/chunk-vendors.04112af6.css"
+  },
+  {
+    "revision": "99af388e1f171f34fb18",
+    "url": "js/chunk-vendors.e3d5dfd2.js"
+  },
+  {
+    "revision": "b838e72fa5f735238fb6",
+    "url": "css/game.72d5657b.css"
+  },
+  {
+    "revision": "b838e72fa5f735238fb6",
+    "url": "js/game.60ee3019.js"
+  },
+  {
+    "revision": "de3002f65fb439dd075fe5852d6d555b",
+    "url": "img/kumamon.de3002f6.png"
+  },
+  {
+    "revision": "e4c2fe4843b1b04fb179d06a02e44383",
+    "url": "index.html"
+  },
+  {
+    "revision": "47567df77cb0c34606bf4c855cba68cb",
+    "url": "browserconfig.xml"
+  },
+  {
+    "revision": "9f5f8e4cd36ac13901ae15dde763ae9c",
+    "url": "site.webmanifest"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "df224f54e21e6d07e8b6db8549042a38",
+    "url": "twicon.css"
+  }
+];
